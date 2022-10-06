04796f4cedfb5fd6e636942aa8e026295e38b501
LearXD <gutyxdcanal@gmail.com>
2022-10-06 20:10:24 -0300

    Att

--- 2º Ano/Lista 3/4.c
+++ 2º Ano/Lista 3/4.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 
-// NÃO FAZ SENTIDO
+void printf4(float *lado, float *perimetro, float *area) {
+    printf("Os dados do quadrado são:\n");
+    printf("\nLado: %.2f", *lado);
+    printf("\nPerimetro: %.2f", *perimetro);
+    printf("\nArea: %.2f", *area);
+}
+
+void main () {
+
+    float lado, perimetro, area;
+    float *plado, *pperimetro, *parea;
+
+    plado = &lado;
+    pperimetro = &perimetro;
+    parea = &area;
+
+    printf("Digite o LADO, PERIMETRO e AREA do quadrado:\n");
+    scanf("%f %f %f", plado, pperimetro, parea);
+
+    printf4(plado, pperimetro, parea);
+}
+
